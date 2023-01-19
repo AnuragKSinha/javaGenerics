0bd6d949c176493cb51c59f169c3a05c0afc17ef
Anurag Sinha <sinhaanurag18@gmail.com>
2023-01-19 09:12:30 +0530

    Add files via upload

---
+++ AutoBoxingWithGenerics.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AutoBoxingWithGenerics {
+	public static void main(String[] args) {
+		int i = 10;
+		Integer x = i; // Autoboxing
+		Integer y = new Integer(10);
+		int j = x; // unboxing
+		List<Integer> list = Arrays.asList(10,20,30);// Autoboxing LHS(Instance of Integer) & RHS (Primitive)
+		int first = list.get(0); // unboxing LHS(Primitive) RHS(Instance of Integer)
+
+		// List<Integer> --> Integer is not a value its a Java Type
+		//https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
+
+		List<String> list1= new ArrayList<>(); // No need to add type on RHS as it will refer LHS
+		list1.add("Hello");
+	}
+}
