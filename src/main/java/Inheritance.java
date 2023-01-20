81773aaa3bdf8ab80b44f964f9cc6c716952dc39
anuragsinha <sinhaanurag18@gmail.com>
2023-01-20 10:53:43 +0530

    inheritance example added

--- src/main/java/Inheritance.java
+++ src/main/java/Inheritance.java
@@ -6,15 +6,27 @@ public class Inheritance {
 		List<String> names=new ArrayList<>();
 		names.add("Name1");
 		names.add("Name2");
-		//printList(names);
+		printList(names);
 		List<Integer> numbers=new ArrayList<>();
 		numbers.add(1);
 		numbers.add(2);
-		//printList(numbers);
+		printList(numbers);
 	}
 
-	private static void printList(List<Object> names) {
+	/**
+	 * In List<Object>, Object is the super class of String and
+	 * Integer. In normal Java concepts Object should have taken in
+	 * String and Integer but in Generics its not support. As
+	 * There is no implications if you are just reading from it.
+	 * Problem is with write.
+	 * There are 2 ways to solve it
+	 * 1. use List --> Non generic type
+	 * 2. use List<?> --> Generic Type which can take any type passed to it
+	 * 		2 has limitations that you can only read and not write
+	 * @param names
+	 */
+	private static void printList(List<?> names) {
 		names.forEach(System.out::println);
-		names.add("Hello");
+		//names.add("Hello");
 	}
 }
